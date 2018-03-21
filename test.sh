d479f30c49709295f9a5960dd73fbb188f063f26
Dennis Loepke <dennis.loepke@hs-bochum.de>
2018-03-21 03:45:38 +0100

    test

--- test.sh
+++ test.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
+
 if [ $1 -eq 0 ]; then
-    echo "TEST"
-  #      IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} citesting_imagedock_1)
-   #     CODE=$(curl -sL -w "%{http_code}" $IP:5000/monster/bla -o /dev/null) || true
-  #      if [ $CODE -ne 200 ]; then
-  #      echo "Site return "
-  #      
-  #      fi
- fi
+ # IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} citesting_imagedock_1) CODE=$(curl -sL -w "%{http_code}" $IP:5000/monster/bla \
+ # dev/null) || true if [ $CODE -ne 200 ]; then echo "Site return " fi fi
+ echo "test";
+fi
