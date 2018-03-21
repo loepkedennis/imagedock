9a920305383776f0d1880bb7bc4f6d5bdc0fd6a0
Dennis Loepke <dennis.loepke@hs-bochum.de>
2018-03-21 02:49:15 +0100

    CI Änderung

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 set -e
   #Run system test if unit test passed
 
-  if [ $1-eq0 ]; then
+  if [ $1 -eq 0 ]; then
         IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} citesting_imagedock_1)
         CODE=$(curl -sL -w "%{http_code}" $IP:5000/monster/bla -o /dev/null) || true
         if [ $CODE -ne 200 ]; then
-        echo "Site return " $CODE
+        echo "Site return "
         ERR=1
         fi
  fi
